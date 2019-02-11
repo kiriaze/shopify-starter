78f765a23869aa823f26163f4b325722646088e2
Constantine <ckiriaze@gmail.com>
2019-02-11 15:56:31 -0800

    clean up

--- config.js
+++ config.js
@@ -40,32 +40,54 @@ module.exports = {
   // match with package.json dependencies
   // $ npm install package --save
   dependencies: {
-    // '$': 'jquery',
-    // 'THREE': 'three'
     '$': 'jquery',
-    '_': 'underscore',
-    'jQuery': 'jquery',
-    'window.jQuery': 'jquery'
-  },
-
-  shopify: {
-    key: process.env.SHOP_KEY,
-    pass: process.env.SHOP_PASSWORD,
-    secret: process.env.SHOP_SECRET,
-    themeId: process.env.SHOP_THEME_ID,
-    shopName: process.env.SHOP_NAME,
-    flatFolders: [
-      'assets',
-      'config',
-      'layout',
-      'locales',
-      'templates'
-    ],
-    multiLevelFolders: [
-      'sections',
-      'snippets'
-    ]
+    // '_': 'underscore',
+    // 'jQuery': 'jquery',
+    // 'window.jQuery': 'jquery'
   }
 
 }
- 
+
+
+
+//'use strict';
+
+// var src   = './',
+//   dist  = './',
+//   assets  = 'assets';
+
+// module.exports = {
+
+//   root: '/',
+
+//   src,
+
+//   dist,
+
+//   assets,
+  
+//   entry: ['app', 'styleguide', 'login'],
+
+//   // match with package.json dependencies
+//   // $ npm install package --save
+//   dependencies: {
+//     '$': 'jquery',
+//     // 'THREE': 'three',
+//     // 'jQuery': 'jquery',
+//     // 'window.jQuery': 'jquery'
+//   },
+
+//   // swap commented out proxy/localhost if needing to access on other devices via ip address
+//   // 'localhost' || '0.0.0.0'
+//   proxy: false,
+//   localhost: '127.0.0.1', // for safari sake..
+//   // localhost: 'localhost',
+//   // proxy: true,
+//   // localhost: '0.0.0.0',
+
+//   port: {
+//     server: 8000,
+//     webpack: 3000
+//   }
+
+// };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,31 +4,36 @@ const path							= require('path');
 const autoprefixer					= require('autoprefixer');
 
 const CopyWebpackPlugin				= require('copy-webpack-plugin');
+const MiniCssExtractPlugin          = require('mini-css-extract-plugin');
 
 let entry = {};
 for (const e of config.entry) entry[e] = `./${config.assets.scripts}/${e}.js`;
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
 	mode: 'production',
 
-	devtool: 'source-map',
+	// devtool: 'source-map',
 
 	optimization: {
 		minimize: true
 	},
 
 	context: path.resolve(__dirname, config.src.root),
+	// context: path.resolve(__dirname, config.src),
 
 	entry: entry,
 
 	output: {
 		path: path.resolve(__dirname, config.dist),
 		filename: 'assets/[name].bundle.js',
+	// 	filename: `${config.assets}/js/[name].bundle.js`
 		publicPath: '/',
 		chunkFilename: 'assets/[name].js'
+	},
+	
+	performance: {
+		hints: "warning"
 	},
 
 	plugins: [
@@ -36,7 +41,10 @@ module.exports = {
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production')
 		}),
-		new ExtractTextPlugin(`assets/[name].bundle.css.liquid`),
+		new MiniCssExtractPlugin({
+			// filename: `${config.assets}/css/[name].bundle.css`
+			filename: `assets/[name].bundle.css.liquid`
+		}),
 		new CopyWebpackPlugin([
 			{
 				flatten: true,
@@ -71,45 +79,30 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.(jpg|png|gif|svg|mp4|mp3)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 8192, // 10000
+					name: '[path][name].[ext]'
+				}
+			},
+			{
+				test: /\.(ttf|eot|woff|woff2)$/,
+				// exclude: /node_modules/,
+				loader: 'url-loader',
+				options: {
+					name: `${config.assets}/fonts/[name].[ext]`
+				}
+			},
+			{
 				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: true,
-								minimize: true
-							}
-						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								plugins: () => [autoprefixer({
-									browsers: ['last 2 versions']
-								})],
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						}
-					]
-				})
-				// // dont use injected style tags but output compiled .css in baseConfig.destPaths.root
-				// use: extractCSS.extract([
-				// 	'css-loader',
-				// 	{
-				// 		loader: 'postcss-loader',
-				// 		options: {
-				// 			plugins: () => [autoprefixer()]
-				// 		}
-				// 	},
-				// 	'sass-loader'
-				// ]),
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
 			},
 			{
 				test: /\.js$/,
@@ -118,27 +111,15 @@ module.exports = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: [
-								'babel-preset-env'
-							]
+							presets: ['@babel/preset-env']
 						}
-					},
-					// 'eslint-loader'
+					}
 				]
 			},
 			{
 				test: /\.html$/,
 				loader: 'html-loader'
 			},
-			{
-				test: /\.(jpg|png|gif|svg|mp4|mp3|ttf|eot|woff|woff2)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 8192,
-					name: '[path][name].[ext]'
-				}
-			}
 		]
 	}
-
 };
